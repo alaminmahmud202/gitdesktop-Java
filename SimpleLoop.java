c50edb14914ccbbe632532d57666cd0b9d985752
A M Mahmud <alaminmahmud684@gmail.com>
2025-03-05 05:28:52 +0600

    Create SimpleLoop.java

---
+++ SimpleLoop.java
@@ -0,0 +1,8 @@
+public class SimpleLoop {
+    public static void main(String[] args) {
+        // Loop from 1 to 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Mahmud: " + i);
+        }
+    }
+}
