81865f5a4499b0f81e6a6fcd4f7e64ffa129814a
Alaminmahmud202 <alaminmahmud684@gmail.com>
2025-03-05 15:16:55 +0600

    SimpleArray.java

---
+++ SimpleArray.java
@@ -0,0 +1,11 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+        // Declare and initialize an array of integers
+        int[] numbers = {10, 20, 30, 40, 50};
+
+
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
