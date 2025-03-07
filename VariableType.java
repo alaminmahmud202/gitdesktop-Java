91c4d4b48657933b2c9ba75142f089a352d75afa
Alaminmahmud202 <alaminmahmud684@gmail.com>
2025-03-07 19:12:45 +0600

    BoolieanIfelse.java

--- VariableType.java
+++ VariableType.java
@@ -1,18 +1,18 @@
 public class VariableType {
-    public VariableType() {
-    }
- 
-    public static void main(String[] var0) {
-       String var1 = "Mahmud";
-       byte var2 = 20;
-       double var3 = 23.44;
-       char var5 = '$';
-       boolean var6 = true;
-       boolean var7 = false;
-       System.out.println(" Hi! I am" + var1);
-       System.out.println(" I am " + var2 + " years old.For java practice my revenue is $" + var3);
-       System.out.println(" My currency is  " + var5);
-       System.out.println(" I am a student and it's : " + var6);
-       System.out.println(" I am a theacher and it's " + var7);
-    }
- }
+   public VariableType() {
+   }
+
+   public static void main(String[] args) {
+      String name = "Mahmud";
+      byte age = 20;
+      double revenue = 23.44;
+      char currency = '$';
+      boolean iamStudent = true;
+      boolean iamTeacher = false;
+      System.out.println(" Hi! I am" + name);
+      System.out.println(" I am " + age + " years old.For java practice my revenue is $" + revenue);
+      System.out.println(" My currency is  " + currency);
+      System.out.println(" I am a student and it's : " + iamStudent);
+      System.out.println(" I am a theacher and it's " + iamTeacher);
+   }
+}
---
+++ booleanifelse.java
@@ -0,0 +1,27 @@
+public class booleanifelse {
+  
+   
+        public static void main(String[] args) {
+            
+    boolean isStudent = true;
+    boolean isOnline = false ;
+    
+    if(isStudent){
+        System.out.println(" You are a student ");
+        }
+        else {
+            System.out.println(" You are not a student");
+        }
+    
+
+    if( isOnline){
+        System.out.println( "You are in online ");
+    }
+else { 
+    System.out.println("You are offline now");
+}
+
+    }
+}
+
+
