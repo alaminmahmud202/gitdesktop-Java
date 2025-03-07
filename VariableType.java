e5987d871cba8d7983615ebb4d615c9765ef4889
Alaminmahmud202 <alaminmahmud684@gmail.com>
2025-03-07 16:38:08 +0600

    VariableType.java

---
+++ VariableType.java
@@ -0,0 +1,18 @@
+public class VariableType {
+    public VariableType() {
+    }
+ 
+    public static void main(String[] var0) {
+       String var1 = "Mahmud";
+       byte var2 = 20;
+       double var3 = 23.44;
+       char var5 = '$';
+       boolean var6 = true;
+       boolean var7 = false;
+       System.out.println(" Hi! I am" + var1);
+       System.out.println(" I am " + var2 + " years old.For java practice my revenue is $" + var3);
+       System.out.println(" My currency is  " + var5);
+       System.out.println(" I am a student and it's : " + var6);
+       System.out.println(" I am a theacher and it's " + var7);
+    }
+ }
