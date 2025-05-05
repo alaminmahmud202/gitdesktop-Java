1fb21b3db12831b8bbad304af11f7c5592ec5731
Alaminmahmud202 <alaminmahmud684@gmail.com>
2025-05-05 17:49:51 +0600

    BioData of a iphone
    phone biodata for my agency

---
+++ BioDataa.java
@@ -0,0 +1,70 @@
+public class BioDataa {
+    public static void main(String[] args) {
+        System.out.println(" \"mahmud's Globe\"");
+        System.out.println("i'm here to work for java programming language");
+        System.out.println("but this was a issue in my vs code editor \ni'm trying to fix this issue");
+        System.out.println("i hope this will be fixed soon");
+
+        int num = 1000000;
+        double num1 = 25000.5;
+        float num2 = 23000.7888f;
+        char ch = 'M';
+        String str = "Mahmud's Globe";
+        double sum = num + num2 + num1;
+       
+        System.out.println("this is a agency. \nName of my agency is " + str);
+        System.out.println("the price of this agency  = " + sum);
+
+// bying a phone for my agency
+        System.out.println("\n\n\ni'm going to buy a phone for my agency");
+        int id = 101;
+        String tittle = " Apple Phone";
+        String brand = "apple incorporated";
+        String colour = "black";
+        int price = 150000;
+        double ip = 108.97849;
+        String model = "iPhone 15 pro max";
+        String os = "ios 17.0";
+        String ram = "8GB"; 
+        String rom = "256GB";
+        String camera = "48MP + 12MP + 12MP";
+        String battery = "4500mAh";
+        String processor = "A17 pro chip";
+        String display = "6.7 inches super retina xdr display";
+        String warranty = "1 year warranty";
+        String sim = "dual sim";
+        String weight = "221 grams";
+        String dimension = "221.2 x 77.6 x 7.4 mm";
+        String network = "5G, 4G, 3G, 2G";
+        String connectivity = "Wi-Fi 6E, Bluetooth 5.3, NFC, Ultra Wideband (UWB)";
+        String features = "Face ID, MagSafe, Ceramic Shield, IP68 water and dust resistance";
+        String accessories = "USB-C to Lightning cable, SIM eject tool, documentation";
+
+
+        System.out.println("this is a phone for my agency. name of my phone is " + tittle);
+        System.out.println("the price of this phone  = " + price);
+        System.out.println("the id of this phone  = " + id);
+        System.out.println("the brand of this phone  = " + brand);
+        System.out.println("the colour of this phone  = " + colour);
+        System.out.println("the ip of this phone  = " + ip);
+        System.out.println("the model of this phone  = " + model);
+        System.out.println("the os of this phone  = " + os);
+        System.out.println("the ram of this phone  = " + ram);
+        System.out.println("the rom of this phone  = " + rom);
+        System.out.println("the camera of this phone  = " + camera);
+        System.out.println("the battery of this phone  = " + battery);
+        System.out.println("the processor of this phone  = " + processor);
+        System.out.println("the display of this phone  = " + display);
+        System.out.println("the warranty of this phone  = " + warranty);
+        System.out.println("the sim of this phone  = " + sim);
+        System.out.println("the weight of this phone  = " + weight);
+        System.out.println("the dimension of this phone  = " + dimension);
+        System.out.println("the network of this phone  = " + network);
+        System.out.println("the connectivity of this phone  = " + connectivity);
+        System.out.println("the features of this phone  = " + features);
+        System.out.println("the accessories of this phone  = " + accessories);
+        System.out.println("the warranty of this phone  = " + warranty);
+        System.out.println("the accessories of this phone  = " + accessories);
+        System.out.println("i'm happy to buy this phone for my agency");
+    }
+}
