4ec50a83adb30e6f7c0f39d1fb865923c9b5f096
Alaminmahmud202 <alaminmahmud684@gmail.com>
2025-05-06 17:59:36 +0600

    ArithmaticDemo.java
    ...

---
+++ ArithmaticDemo.java
@@ -0,0 +1,13 @@
+public class ArithmaticDemo{ 
+public static void main(String[] args){
+int a = 10;
+
+int b = 5;
+system.out.println ( "Addition of a and b is" + ( a+b ));
+system.out.println ( "Subtraction of a and b is" + ( a-b ));
+system.out.println ( "Multiplication of a and b is" + ( a*b ));
+system.out.println ( "Division of a and b is" + ( a/b ));
+system.out.println ( "Modulus of a and b is" + ( a%b ));
+
+}
+} 
